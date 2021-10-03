69e572e46cf665b827a92f456009bd3b7e9fc2e8
danz <simaodeveloper@gmail.com>
2021-10-02 23:22:57 -0300

    feat(modules): make container fetch mount methods from modules

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,10 @@
-import 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/modules';
+import { mount as productsMount } from 'products/modules';
+
+const cartContainer = document.querySelector('[data-remote-id="cart"]');
+const productsContainer = document.querySelector('[data-remote-id="products"]');
+
+cartMount(cartContainer);
+productsMount(productsContainer);
 
 console.log('Container!');
